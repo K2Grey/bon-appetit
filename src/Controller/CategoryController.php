701a4b9d7baa4da33e3ea2280a3158f2850379e4
Grégory TROUPENAT <gregory.troupenat@gmail.com>
2021-02-04 18:41:19 +0100

    crud created for Recipe and Category entities. Recipe and Category controllers updated.

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use App\Repository\CategoryRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/categories", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('category/index.html.twig', [
+            'categories' => $categoryRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{categorySlug}", name="show", methods={"GET"})
+     * @ParamConverter("category", options={"mapping": {"categorySlug": "slug"}})
+     * @param Category $category
+     * @return Response
+     */
+    public function show(Category $category): Response
+    {
+        return $this->render('category/show.html.twig', [
+            'category' => $category,
+        ]);
+    }
+
+    /**
+     * @Route("/{categorySlug}/edit", name="edit", methods={"GET","POST"})
+     * @ParamConverter("category", options={"mapping": {"categorySlug": "slug"}})
+     * @param Request $request
+     * @param Category $category
+     * @return Response
+     */
+    public function edit(Request $request, Category $category): Response
+    {
+        $form = $this->createForm(CategoryType::class, $category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/edit.html.twig', [
+            'category' => $category,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{categorySlug}", name="delete", methods={"DELETE"})
+     * @ParamConverter("category", options={"mapping": {"categorySlug": "slug"}})
+     * @param Request $request
+     * @param Category $category
+     * @return Response
+     */
+    public function delete(Request $request, Category $category): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($category);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('category_index');
+    }
+}
---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use App\Repository\RecipeRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/recipe", name="recipe_")
+ */
+class RecipeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     * @param RecipeRepository $recipeRepository
+     * @return Response
+     */
+    public function index(RecipeRepository $recipeRepository): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'recipes' => $recipeRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        $recipe = new Recipe();
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($recipe);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('recipe_index');
+        }
+
+        return $this->render('recipe/new.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{recipeSlug}", name="show", methods={"GET"})
+     * @ParamConverter("recipe", options={"mapping": {"recipeSlug": "slug"}})
+     * @param Recipe $recipe
+     * @return Response
+     */
+    public function show(Recipe $recipe): Response
+    {
+        return $this->render('recipe/show.html.twig', [
+            'recipe' => $recipe,
+        ]);
+    }
+
+    /**
+     * @Route("/{recipeSlug}/edit", name="edit", methods={"GET","POST"})
+     * @ParamConverter("recipe", options={"mapping": {"recipeSlug": "slug"}})
+     * @param Request $request
+     * @param Recipe $recipe
+     * @return Response
+     */
+    public function edit(Request $request, Recipe $recipe): Response
+    {
+        $form = $this->createForm(RecipeType::class, $recipe);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('recipe_index');
+        }
+
+        return $this->render('recipe/edit.html.twig', [
+            'recipe' => $recipe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{recipeSlug}", name="delete", methods={"DELETE"})
+     * @ParamConverter("recipe", options={"mapping": {"recipeSlug": "slug"}})
+     * @param Request $request
+     * @param Recipe $recipe
+     * @return Response
+     */
+    public function delete(Request $request, Recipe $recipe): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$recipe->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($recipe);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('recipe_index');
+    }
+}
