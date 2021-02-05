d396f93e060a0f573f1a4ecddf4a71f2f130b92a
Grégory TROUPENAT <gregory.troupenat@gmail.com>
2021-02-05 01:46:44 +0100

    navbar updated

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\DataFixtures\CategoryFixtures;
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,6 +21,17 @@ class HomeController extends AbstractController
     {
         return $this->render('home/index.html.twig', [
             'recipes' => $recipeRepository->lastRecipesAdded(),
+        ]);
+    }
+
+    /**
+     * @param CategoryRepository $categoryRepository
+     * @return Response
+     */
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('_navbar.html.twig', [
+            'categories' => $categoryRepository->findAll(),
         ]);
     }
 }
